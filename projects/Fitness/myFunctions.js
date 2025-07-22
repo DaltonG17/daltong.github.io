5df60b9173dbc1d97ba614036ce919253f808f52
DaltonG17 <dgrnwd04@gmail.com>
2025-07-22 13:59:36 -0500

    Add files via upload

---
+++ projects/Fitness/myFunctions.js
@@ -0,0 +1,26 @@
+/* Dalton Greenwood
+November 8, 2021
+My Function List
+*/
+
+//Thank you alert
+function tyAlert() {
+	alert("Thank you for your interest in our fitness club. We will contact you soon.");
+}
+
+// Opens a popup window with the Burpees workout video
+function burpees() {
+	window.open("scripts/scripts/burpees.html", "_blank", "width=610, height=360");
+}
+
+// Opens a popup window with the Plank workout video
+function plank() {
+	window.open("scripts/scripts/plank.html", "_blank", "width=610, height=360");
+}
+
+// Opens a popup window with the Mountain Climber workout video
+	function mtnClimber() {
+		window.open("scripts/scripts/mtn.html", "_blank", "width=610, height=360");
+	}
+
+	
